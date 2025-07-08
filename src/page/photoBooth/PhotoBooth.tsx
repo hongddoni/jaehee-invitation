94b7f1b5d98d051d21e03580b23b5391a5e419bd
hongddoni <tmdgur122@naver.com>
2025-07-08 20:43:59 +0900

    feat: 사진 좌우버튼 추가

--- src/page/photoBooth/PhotoBooth.tsx
+++ src/page/photoBooth/PhotoBooth.tsx
@@ -13,10 +13,54 @@ export const PhotoBooth = ({ images }: Props) => {
 		setSelectedId(index);
 	};
 
+	const onLeftClick = () => {
+		if (selectedId === 0) {
+			setSelectedId(images.length - 1);
+		} else {
+			setSelectedId(selectedId - 1);
+		}
+	};
+
+	const onRightClick = () => {
+		if (selectedId === images.length - 1) {
+			setSelectedId(0);
+		} else {
+			setSelectedId(selectedId + 1);
+		}
+	};
+
 	return (
 		<div className={s.photoBooth}>
 			<div className={s.selectedImage}>
+				<button
+					onClick={onLeftClick}
+					className={classNames(s.button, s.left)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="48px"
+						viewBox="0 -960 960 960"
+						width="24px"
+						fill="white"
+					>
+						<path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+					</svg>
+				</button>
 				<img src={images[selectedId]} alt={`wedding-${selectedId}`} />
+				<button
+					onClick={onRightClick}
+					className={classNames(s.button, s.right)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="48px"
+						viewBox="0 -960 960 960"
+						width="24px"
+						fill="white"
+					>
+						<path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+					</svg>
+				</button>
 			</div>
 			<div className={s.photoList}>
 				{images.map((image, index) => (
